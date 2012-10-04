6770898ce96f59afec09f646f9cedd436f793d71
Andrew Xu <andrew.xu@istockphoto.com>
2012-10-04 13:29:20 -0600

    Recreated rails app due to corrupt rails installation

--- blogger/config/initializers/secret_token.rb
+++ blogger/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blogger::Application.config.secret_token = '413fd1320acf43d5870ee0c423f943892fc1fa418974e0d1cf6e2675753419024e44183213bf3b77cf4040bc05f8a30948feeb236373cc63d32b8a1a9f6c36c8'
+Blogger::Application.config.secret_token = '9a8b99059998b1c1ed39ed449d75db1d876a715f32a5f44235d44e10c5faa6c5b0532046d57d1d184c40dd32924eaa252aac932ca72f9d8f7ea00df4d0b6ce31'
