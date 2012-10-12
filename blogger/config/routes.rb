e5eea714b8a9a999d629f386f623e8f8080e5b33
Andrew Xu <andrew.xu@istockphoto.com>
2012-10-12 09:17:09 -0600

    Committing code after Iteration 3 before starting I4

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,5 +1,7 @@
 Blogger::Application.routes.draw do
   resources :articles
+  resources :comments
+  resources :tags
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
