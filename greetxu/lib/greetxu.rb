dfa3c9c2087b4c82a25a4ff048af58b23ece2db4
Andrew Xu <andrew.xu@istockphoto.com>
2012-10-29 08:45:29 -0600

    Modifying Gem
    modified the gem to not use dependencies (lib) files due to the fact that I'm not publishing
    this gem, so that anyone can use it locally without hacking the lib directory

--- greetxu/lib/greetxu.rb
+++ greetxu/lib/greetxu.rb
@@ -1,8 +1,22 @@
 require "greetxu/version"
-require "greetxu/message"
 
 # The main Greetxu driver
 module Greetxu
+    # Random Messages for guest
+    GUEST_MESSAGE = [
+        "Welcome, please login to get started",
+        "To begin, please login",
+        "Hey guest!  Click login to begin"
+    ]
+
+    # Random Messages for user
+    USER_MESSAGE = [
+        "Hello %s",
+        "Hi %s",
+        "How are you %s",
+        "Welcome, %s"
+    ]
+
     # Randomizes a greeting message depending on whether a name has been passed in
     #
     # Example:
